4178b31d64df486dc20c125db67aa45225e1f901
tomohiro021 <siruforia@gmail.com>
2020-04-24 15:10:10 +0000

    ルーティング変更

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('main','MainController@index');
 
 Auth::routes();
 
+Route::get('/', 'HomeController@index');
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('main','MainController@index');
